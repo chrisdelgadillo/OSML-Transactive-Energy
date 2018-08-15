74445c780421d16c3988e947e8c8a6512070b546
natewise <32283998+natewise@users.noreply.github.com>
2018-08-14 18:00:28 -0700

    Add files via upload

--- transactive-energy-hlc/transaction-logic.js
+++ transactive-energy-hlc/transaction-logic.js
@@ -10,20 +10,18 @@ async function transaction (Transaction) //Transaction is our transaction create
  {
  	const contract = Transaction.electricity.contract; //this is the contract we defined within our asset
  	const electricity = Transaction.electricity;//variable that represents electricity function with all of it's parameters
- 	let transactionOutput = contract.unitPrice * electricity.unitCount; //variable represents transaction output
+ 	let payout = contract.unitPrice * electricity.unitCount; //variable represents transaction payout
     
- 	console.log('Received at: ' + contract.timestamp); //print out timestamp
-    console.log('Contract arrivalDateTime: ' + contract.arrivalDateTime); //print out arrival Date
+ 	console.log('Received at: ' + contract.timestamp); //print out timestamp (the time at which the transaction was made)
 
     //Set status of shipment
-    electricity.status = 'Arrived';
+	electricity.status = 'COMPLETED';
+	
+	/*
+	if(seller.batteryWattage >= electricity.unitCount)
+	if(buyer.accountBalance >= payout)
+	*/
 
-    //if shipment did not arrive, set payment to 0
-    if(Transaction.timestamp > contract.arrivalDateTime) 
-    	 {
-    	 	transcriptOutput = 0;
-    	 	console.log('Late shipment');
-    	 }
    
  }
 
